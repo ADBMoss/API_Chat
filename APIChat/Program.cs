b4449cdf8c110ca1cbdd8be4fbc837ecfa4c748f
DarriusB <darrius.bradford@gmail.com>
2022-05-25 07:53:00 -0700

    Exercise is complete.

---
+++ APIChat/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace APIChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var quote = new Quotes(client);
+
+            Console.WriteLine($"Conversation Between Kanye West & Ron Swanson:");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Kanye: {quote.KanyeWest()}");
+                Console.Write($"Ron Swanson: {quote.RonSwanson()}");
+
+                if (i < 4)
+                {
+                    Console.WriteLine($"\n------------------------------------");
+                    Console.WriteLine("Press Enter to Continue Conversation");
+                    Console.WriteLine($"------------------------------------\n");
+                    Console.ReadKey();
+                    }
+                else
+                {
+                    Console.WriteLine($"\n-------------------");
+                    Console.WriteLine("End of Conversation");
+                    Console.WriteLine($"-------------------\n");
+                    Console.ReadKey();
+
+                }
+
+            }
+
+            
+
+
+        }
+    }
+}
---
+++ APIChat/Quotes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace APIChat
+{
+    public class Quotes
+    {
+        private HttpClient _client;
+
+        public Quotes(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public string KanyeWest()
+        {
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest/";
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            string newAnswer = "";
+            foreach (var item in kanyeQuote)
+            {
+                char answer2 = '"';
+                newAnswer = kanyeQuote.Replace(answer2, ' ').Replace('}', ' ').Trim();
+            }
+
+            return newAnswer;
+        }
+
+        public string RonSwanson()
+        {
+            string newAnswer2 = "";
+            var client2 = new HttpClient();
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client2.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString();
+            foreach (var item2 in ronQuote)
+            {
+                char answer2 = '"';
+                newAnswer2 = ronQuote.Replace(answer2, ' ').Replace('[', ' ').Replace(']', ' ').TrimStart();
+            }
+
+            return newAnswer2;
+        }
+    }
+}
